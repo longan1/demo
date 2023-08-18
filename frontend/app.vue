f5ca632fdc4ef188067bf708ab92da6532cc20b4
dattt <dattt@syn-gr.com>
2023-08-18 22:25:34 +0700

    u

--- frontend/app.vue
+++ frontend/app.vue
@@ -3,3 +3,11 @@
     <NuxtWelcome />
   </div>
 </template>
+<script setup lang="ts">
+      const result = useFetch('http://localhost.com/api', {
+        lazy: true,
+        server: false
+      })
+      console.log(result);
+      
+</script>
