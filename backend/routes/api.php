b40d0ed237da3004e72525023dbc6dfc61090c72
dattt <dattt@syn-gr.com>
2023-08-19 14:41:22 +0700

    setup docker

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',function(Request $request){
-   return 'hello'
+   return 'hello';
 });
---
+++ dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Navigate to the backend directory
+cd ./backend
+# Check if the .env file exists
+if [ ! -f .env ]; then
+    echo "Not found .env in the backend directory. Exiting..."
+    exit 1
+fi
+# Run docker-compose up for the backend
+docker-compose up -d
+
+# Navigate back to the main directory
+cd ..
+
+# Navigate to the frontend directory
+cd ./frontend
+
+# Run docker-compose up for the frontend
+docker-compose up -d
